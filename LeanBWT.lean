eb973e2bb13533800d264a39b9f0154bbac83a6f
AnirudhG07 <anirudhgupta@iisc.ac.in>
2026-05-05 12:37:26 +0530

    bwt init

---
+++ LeanBWT.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `LeanBWT` library.
+-- Import modules here that should be built as part of the library.
+import LeanBWT.BWT
+import LeanBWT.BwtCorrectness
---
+++ README.md
@@ -0,0 +1,1 @@
+# LeanBWT
